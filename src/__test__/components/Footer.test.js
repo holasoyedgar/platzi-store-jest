011831c395f1d492a3412dc83b370243f6333433
Edgar Rincon <eers222@gmail.com>
2021-04-08 22:55:36 -0500

    Hace uso de snapshots

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 
 import Footer from '../../components/Footer';
 
@@ -10,5 +11,12 @@ describe('<Footer/>', () => {
   });
   test('Render del título', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
